fe55b651977f38718a1d1c70165cd39ce4899619
fensken <shardul.gautam123@gmail.com>
2024-09-10 02:01:15 +0545

    fix: build.

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,17 +1,28 @@
-import * as admin from "firebase-admin";
+// Import the functions you need from the SDKs you need
+import { App, cert, getApp, getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-      }),
-    });
-  } catch (error) {
-    console.log("Firebase admin initialization error", error);
-  }
+// Your web app's Firebase configuration
+
+const serviceKey = require("@/service_key.json");
+
+let app: App;
+
+// Initialize Firebase
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  });
+} else {
+  app = getApp();
 }
 
-export default admin;
+const adminDb = getFirestore(app);
+
+export { app as adminApp, adminDb };
