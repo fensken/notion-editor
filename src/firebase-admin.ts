b743461093eb6b629ca6733bf6e949d503282e68
fensken <shardul.gautam123@gmail.com>
2024-09-10 01:48:13 +0545

    fix: firebase key.

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,24 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { App, cert, getApp, getApps, initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import admin from "firebase-admin";
 
-// Your web app's Firebase configuration
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY!);
 
-const serviceKey = require("./service_key.json");
-
-let app: App;
-
-// Initialize Firebase
-if (getApps().length === 0) {
-  app = initializeApp({
-    credential: cert(serviceKey),
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
   });
-} else {
-  app = getApp();
 }
 
-const adminDb = getFirestore(app);
-
-export { app as adminApp, adminDb };
+export default admin;
